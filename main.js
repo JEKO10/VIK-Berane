1f5c90da6232e2c7dd8b03d1cb635a7f5701fab7
Aleksa Bubanja <aleksa3b@gmail.com>
2023-01-27 21:47:55 +0100

    removed customers page

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const menuBtn = document.querySelector(".menuBtn");
 const menuBurger = document.querySelector(".menuBurger");
 const navList = document.querySelector("nav ul");
+const submit = document.querySelector("#submit");
+const message = document.querySelector("#message");
 
 menuBtn.addEventListener("click", () => {
   menuBurger.classList.toggle("open");
   navList.classList.toggle("hide");
   document.body.classList.toggle("hidden");
 });
-
-const submit = document.querySelector("#submit");
-const message = document.querySelector("#message");
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
